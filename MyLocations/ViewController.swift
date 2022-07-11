7e1352c0471669f7e560c92c0b517920d39d3bcf
Gospodi <gospodijazzet@gmail.com>
2022-07-11 21:02:40 +0300

    Initial Commit

---
+++ MyLocations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by Gospodi on 11.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
