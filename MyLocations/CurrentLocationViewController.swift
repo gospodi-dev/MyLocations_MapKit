8770629e45c049af115e48eddf246ec839ab61e3
Gospodi <gospodijazzet@gmail.com>
2022-07-11 21:37:15 +0300

    добавлены переменные для подключения к интерфейсу

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class CurrentLocationViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+    
+    // MARK: - Actions
+    @IBAction func getLocation() {
+        // do nothing yet
+    }
 }
 
