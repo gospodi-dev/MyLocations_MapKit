a917318425c8f6eeba195221fd6b945f20522586
Gospodi <gospodijazzet@gmail.com>
2022-07-18 23:08:23 +0300

    добавлен новый контроллер представления

---
+++ MyLocations/LocationDetailsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LocationDetailsViewController.swift
+//  MyLocations
+//
+//  Created by Gospodi on 18.07.2022.
+//
+
+import UIKit
+
+class LocationDetailsViewController: UITableViewController {
+  @IBOutlet var descriptionTextView: UITextView!
+  @IBOutlet var categoryLabel: UILabel!
+  @IBOutlet var latitudeLabel: UILabel!
+  @IBOutlet var longitudeLabel: UILabel!
+  @IBOutlet var addressLabel: UILabel!
+  @IBOutlet var dateLabel: UILabel!
+
+  // MARK: - Actions
+  @IBAction func done() {
+    navigationController?.popViewController(animated: true)
+  }
+
+  @IBAction func cancel() {
+    navigationController?.popViewController(animated: true)
+  }
+}
