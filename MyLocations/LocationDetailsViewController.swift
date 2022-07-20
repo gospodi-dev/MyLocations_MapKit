6c0bce2b046468d0f3157f6df224b4adb7bd31ea
Gospodi <gospodijazzet@gmail.com>
2022-07-20 22:22:49 +0300

    добавлены переменные для отображения информации о местоположении

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -6,21 +6,28 @@
 //
 
 import UIKit
+import CoreLocation
 
 class LocationDetailsViewController: UITableViewController {
-  @IBOutlet var descriptionTextView: UITextView!
-  @IBOutlet var categoryLabel: UILabel!
-  @IBOutlet var latitudeLabel: UILabel!
-  @IBOutlet var longitudeLabel: UILabel!
-  @IBOutlet var addressLabel: UILabel!
-  @IBOutlet var dateLabel: UILabel!
-
-  // MARK: - Actions
-  @IBAction func done() {
-    navigationController?.popViewController(animated: true)
-  }
-
-  @IBAction func cancel() {
-    navigationController?.popViewController(animated: true)
-  }
+    @IBOutlet var descriptionTextView: UITextView!
+    @IBOutlet var categoryLabel: UILabel!
+    @IBOutlet var latitudeLabel: UILabel!
+    @IBOutlet var longitudeLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+    
+    var coordinate = CLLocationCoordinate2D(
+      latitude: 0,
+      longitude: 0)
+    var placemark: CLPlacemark?
+    
+    // MARK: - Actions
+    @IBAction func done() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func cancel() {
+        navigationController?.popViewController(animated: true)
+    }
 }
